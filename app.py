8ae534a1f1d008d5268c37d8866a07630fbc1ab4
o-seonsik <dhtjstlr777@gmail.com>
2020-11-12 22:04:30 +0900

    Add login function

--- app.py
+++ app.py
@@ -27,5 +27,19 @@ def join():
 
     return jsonify({"code": 0, "msg": "Join success"})
 
+
+@app.route('/login', methods=['POST'])
+def login():
+    req = request.get_json()
+    mem_list = db.member
+
+    result = mem_list.find({'IMEI': req['IMEI']}).count()
+
+    if result == 1:
+        return jsonify({"code": 0, "msg": "Login success"})
+    else:
+        return jsonify({"code": 1, "msg": "Failed login"}), 401
+
+
 if __name__ == '__main__':
     app.run()
